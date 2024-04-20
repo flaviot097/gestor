f0265126f77ce7edc86ce6dc748bf7a673767295
flaviot097 <112975530+flaviot097@users.noreply.github.com>
2024-04-20 20:06:40 -0300

    configuracion de tablas de la db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,9 +13,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('id')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name-product');
+            $table->string('id')->unique();
+            $table->timestamp('publication')->nullable();
+            $table->string('description');
+            $table->integer('stock');
             $table->rememberToken();
             $table->timestamps();
         });
